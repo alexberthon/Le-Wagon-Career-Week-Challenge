11f87b224e4f37369a2e338d3ca395cdb4bcdc53
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-06-22 14:18:15 +0200

    create mission after a reservation is created

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,18 @@
 class Reservation < ApplicationRecord
   belongs_to :listing
+
+  after_create :create_mission
+
+  private
+
+  def create_mission
+    return if listing.missions.where(mission_type: "last_checkout", date: end_date).exists?
+
+    Mission.create!(
+      listing: listing,
+      mission_type: "checkout_checkin",
+      date: start_date,
+      price: 10 * listing.num_rooms
+    )
+  end
 end
