3cf67cdd1643f28512f37adc647500dd043d3f60
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-06-22 13:59:43 +0200

    add create, update, destroy actions to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,5 @@
-# require "./app/serializers/listings_serializer"
-
 class ListingsController < ApplicationController
-  before_action :set_listing, only: %i[show update delete]
+  before_action :set_listing, only: %i[show update destroy]
 
   def index
     @listings = Listing.all
@@ -9,12 +7,38 @@ class ListingsController < ApplicationController
   end
 
   def show
-    render json: @listing, serializer: ListingsSerializer
+    render json: @listing, serializer: ListingsSerializer, adapter: :json
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      render json: @listing, serializer: ListingsSerializer, adapter: :json
+    else
+      render json: { error: "Unprocessable params" }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @listing.num_rooms = listing_params[:num_rooms]
+    if @listing.save
+      render json: @listing, serializer: ListingsSerializer, adapter: :json
+    else
+      render json: { error: "Unprocessable params" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @listing.destroy
   end
 
   private
 
   def set_listing
     @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:num_rooms)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :listings, only: %i[index show create update delete]
+  resources :listings, only: %i[index show create update destroy]
   resources :missions, only: %i[index]
 end
