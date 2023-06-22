d53d1aa4145e8468e11f7dfd2606fdfe2f21f9cf
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-06-22 14:00:13 +0200

    update missions after listing update

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,20 @@
 class Listing < ApplicationRecord
-  has_many :bookings
-  has_many :reservations
-  has_many :missions
+  has_many :bookings, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_many :missions, dependent: :destroy
 
+  validates :num_rooms, numericality: { only_integer: true }
+
+  after_update :update_missions
+
+  private
+
+  def update_missions
+    missions.each do |mission|
+      mission.price = 10 * num_rooms if mission.mission_type == "first_checkin"
+      mission.price = 10 * num_rooms if mission.mission_type == "checkout_checkin"
+      mission.price = 5 * num_rooms if mission.mission_type == "last_checkout"
+      mission.save
+    end
+  end
 end
